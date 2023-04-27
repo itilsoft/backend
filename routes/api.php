477eabc9d47099f1a1576e69240e32ad4eed6957
ysumut <umut_aslan95@hotmail.com>
2023-04-27 23:51:52 +0300

    user/login route converted to post request

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/user/login', function (Request $request) {
+Route::post('/user/login', function (Request $request) {
     $user = \App\Models\User::query()->where('email', '=','umut@gmail.com')->first();
     $token = $user->createToken('itilsoft');
 
