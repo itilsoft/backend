d4f9233c36c1258e777a8ca27a220eaa26bb1e7e
ysumut <umut_aslan95@hotmail.com>
2023-05-21 01:08:32 +0300

    admin endpoint added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Resource;
+use App\Models\Comment;
 use App\Models\User;
+use Illuminate\Support\Carbon;
 
 class AdminController extends Controller
 {
     public function getStatistics()
     {
-        return "admin";
+        return (new Resource(true))->response(['data' => [
+            'users' => $this->getUsers(),
+            'comments' => $this->getComments(),
+        ]]);
+    }
+
+    private function getUsers() {
+        $data = [];
+
+        for ($i = 6; $i >= 0; $i--) {
+            $date = Carbon::now()->subDays($i)->toDateString();
+            $userCount = User::whereDate('created_at', $date)->count();
+
+            $data[] = [
+                'date' => $date,
+                'count' => $userCount,
+            ];
+        }
+
+        return $data;
+    }
+
+    private function getComments() {
+        $data = [];
+
+        for ($i = 6; $i >= 0; $i--) {
+            $date = Carbon::now()->subDays($i)->toDateString();
+            $commentCount = Comment::whereDate('created_at', $date)->count();
+            $averageStar = Comment::whereDate('created_at', $date)->avg('star');
+
+            $data[] = [
+                'date' => $date,
+                'count' => $commentCount,
+                'average_star' => $averageStar ? round($averageStar, 2) : 0,
+            ];
+        }
+
+        return $data;
     }
 }
