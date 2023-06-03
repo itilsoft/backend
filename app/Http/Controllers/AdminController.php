52b11f277e527a5ff4495182b1506f315eb19b77
ysumut <umut_aslan95@hotmail.com>
2023-06-04 00:50:50 +0300

    admin statistics fixed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Resource;
 use App\Models\Comment;
+use App\Models\Service;
 use App\Models\User;
 use Illuminate\Support\Carbon;
 
@@ -11,43 +12,87 @@ class AdminController extends Controller
 {
     public function getStatistics()
     {
-        return (new Resource(true))->response(['data' => [
-            'users' => $this->getUsers(),
-            'comments' => $this->getComments(),
-        ]]);
+        $services = $this->getServices();
+
+        return (new Resource(true))->response([
+            'data' => [
+                'averageRatingOfAllServices' => $services['averageRatingOfAllServices'],
+                'mostCommentingServices' => $services['mostCommentingServices'],
+                'mostRatingServices' => $services['mostRatingServices'],
+                'users' => $this->getUsers(),
+                'comments' => $this->getComments(),
+            ]]);
     }
 
-    private function getUsers() {
-        $data = [];
+    private function getServices()
+    {
+        $services = Service::with('comments')->get();
+        $totalStar = 0;
+
+        foreach ($services as $s) {
+            $s->commentsCount = $s->comments->count();
+            $s->averageStar = $s->averageStar();
+            $totalStar += $s->averageStar;
+            unset($s->comments);
+        }
+
+        return [
+            'averageRatingOfAllServices' => round($totalStar / $services->count(), 2),
+            'mostCommentingServices' => $services->sortByDesc('commentsCount')->values(),
+            'mostRatingServices' => $services->sortByDesc('averageStar')->values(),
+        ];
+    }
+
+    private function getUsers()
+    {
+        $result = [];
+        $days = [];
 
         for ($i = 6; $i >= 0; $i--) {
-            $date = Carbon::now()->subDays($i)->toDateString();
-            $userCount = User::whereDate('created_at', $date)->count();
+            $days[] = Carbon::now()->subDays($i);
+        }
 
-            $data[] = [
-                'date' => $date,
-                'count' => $userCount,
+        $users = User::query()->whereBetween('created_at', [
+            $days[0]->startofDay()->toDateTimeString(),
+            $days[count($days) - 1]->endOfDay()->toDateTimeString()
+        ])->get();
+
+        foreach ($days as $d) {
+            $result[] = [
+                'date' => $d->toDateString(),
+                'count' => $users->whereBetween('created_at', [
+                    $d->startofDay()->toDateTimeString(), $d->endOfDay()->toDateTimeString()
+                ])->count(),
             ];
         }
 
-        return $data;
+        return $result;
     }
 
-    private function getComments() {
-        $data = [];
+    private function getComments()
+    {
+        $result = [];
+        $days = [];
 
         for ($i = 6; $i >= 0; $i--) {
-            $date = Carbon::now()->subDays($i)->toDateString();
-            $commentCount = Comment::whereDate('created_at', $date)->count();
-            $averageStar = Comment::whereDate('created_at', $date)->avg('star');
+            $days[] = Carbon::now()->subDays($i);
+        }
 
-            $data[] = [
-                'date' => $date,
-                'count' => $commentCount,
-                'average_star' => $averageStar ? round($averageStar, 2) : 0,
+        $comments = Comment::query()->whereBetween('created_at', [
+            $days[0]->startofDay()->toDateTimeString(),
+            $days[count($days) - 1]->endOfDay()->toDateTimeString()
+        ])->get();
+
+        foreach ($days as $d) {
+            $commentsOfDay = $comments->whereBetween('created_at', [
+                $d->startofDay()->toDateTimeString(), $d->endOfDay()->toDateTimeString()
+            ]);
+            $result[] = [
+                'date' => $d->toDateString(),
+                'count' => $commentsOfDay->count(),
             ];
         }
 
-        return $data;
+        return $result;
     }
 }
